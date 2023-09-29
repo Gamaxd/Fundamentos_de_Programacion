99edb256d4a20f93fb8219a93c12484a9bf05f17
Gamaxd <L23180789@slp.tecnm.mx>
2023-09-29 12:07:14 -0900

    Create Programa2.cs

---
+++ Unidad 2/Programa2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Programa2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Gamaliel ", apellidos = "Zapata Alonso", completo;
+            completo = nombre + apellidos;
+            int a = 5, b = 23, suma;
+            suma = a + b;
+            Console.WriteLine( " * * Bienvenido * * " + completo + " * *" );
+            Console.Write( "la operacion suma de A=" + a + " B=" + b );
+            Console.Write( $"Tu resultado es {suma}" );
+            Console.WriteLine( " -- Gracias : )"); 
+        }
+    }
+}

346a6bfbb5f5b0e00a4d38d05827bd8ae8b93c4d
Gamaxd <L23180789@slp.tecnm.mx>
2023-09-29 12:10:30 -0900

    xd

---
+++ Unidad 2/Programa4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace practica_4 // Note: actual namespace depends on the project 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Y, A;
+            float suma;
+
+            Console.WriteLine("Escriba el valor de la variable Y: ");
+            Y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba el valor de la variable A: ");
+            A = Convert.ToInt32(Console.ReadLine());
+            //Paso calcular la Suma.
+            suma = (Y + A + 3);
+            //Paso imprime el resultado.
+            Console.WriteLine("El resultado de la suma es: " + suma);
+        }
+    }
+}
---
+++ Unidad 2/Programa5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace programa5 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int B, H;
+            float multiplicacion;
+
+            Console.WriteLine("Escriba el valor de la variable B: ");
+            B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba el valor de la variable H: ");
+            H = Convert.ToInt32(Console.ReadLine());
+            //paso calcular la operacion
+            multiplicacion = (B + H) / 2;
+            Console.WriteLine("El resultado de la operacion es: " + multiplicacion);
+
+            int L1, L2, L3;
+            float suma;
+            Console.WriteLine("Escriba el valor de la variable L1: ");
+            L1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba el valor de la variable L2: ");
+            L2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba el valor de la variable L3: ");
+            L3 = Convert.ToInt32(Console.ReadLine());
+            //paso calcular la operacion
+            suma = (L1 + L2 + L3);
+            Console.WriteLine("El resultado de la operacion es: " + suma);
+        }
+    }
+}

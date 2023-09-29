a14044e918217e1e882a64c2664821508d46a7a9
Gamaxd <L23180789@slp.tecnm.mx>
2023-09-29 11:57:45 -0900

    Mi primer programa

---
+++ Unidad 2/Programa1.cs
@@ -0,0 +1,16 @@
+﻿using System;// paso 1
+
+namespace p_1// paso 1 
+{
+    internal class Program// paso 1 
+    {
+        static void Main(string[] args) // paso 1 
+        {
+            //DECLARAR VARIABLES
+            string nombre = "Gamaliel Salvador"; //PASO 2
+            Console.WriteLine(nombre); //PASO 3
+            //cada linea debe finalizar con; punto y coma
+        }
+        //paso 4
+    }//paso 4 
+}//paso 4

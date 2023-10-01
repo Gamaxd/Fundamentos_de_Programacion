4a96a9a2fe2cbf3fce763c5714178a8b18af6b63
Gamaxd <L23180789@slp.tecnm.mx>
2023-09-30 18:43:22 -0600

    Create Programa8.cs

---
+++ Unidad 2/Programa8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Programa8 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int P1, P2, P3, IVA = 16;
+            float multiplicacion, suma;
+            Console.WriteLine("Escriba el valor del producto 1: ");
+            P1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba el valor del producto 2: ");
+            P2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba el valor del producto 3: ");
+            P3 = Convert.ToInt32(Console.ReadLine());
+            suma = (P1 + P2 + P3);
+            Console.WriteLine("La suma de los productos es: " + suma);
+            multiplicacion = (P1 + P2 + P3) * IVA;
+            Console.WriteLine("El resultado del producto con IVA es: " + multiplicacion);
+        }
+    }
+}

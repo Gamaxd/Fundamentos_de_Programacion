d2c17ba911d04c63511a5869f9e3e64710b3e4c5
Gamaxd <L23180789@slp.tecnm.mx>
2023-09-30 18:44:48 -0600

    Create Programa10.cs

---
+++ Unidad 2/Programa10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Programa10 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Perimetro, Apotema;
+            float multiplicacion;
+            Console.WriteLine("Asigna el Perimetro: ");
+            Perimetro = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Asigna la Apotema: ");
+            Apotema = Convert.ToInt32(Console.ReadLine());
+            multiplicacion = (Perimetro * Apotema) / 2;
+
+            Console.WriteLine("El area del hexagono es: " + multiplicacion);
+        }
+    }
+}

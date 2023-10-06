a820f9e85d8ab857ab1b61ae1500dcd5f802005b
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-05 19:54:16 -0600

    Add files via upload

---
+++ Unidad 2/Programa6.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa6;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa6 {
+
+    public static void main(String[] args) {
+        Scanner it = new Scanner(System.in); 
+        double p1, p2, p3, subtotal, total, ivaT;
+        final double iva = 0.16;
+        System.out.println("Escribe el precio 1: ");
+        p1 = it.nextDouble();
+        System.out.println("Escribe el precio 2: ");
+        p2 = it.nextDouble();
+        System.out.println("Escribe el precio 3: ");
+        p3 = it.nextDouble();
+        total = p1 + p2 + p3;
+        ivaT = total * iva;
+        subtotal = total = ivaT;
+        System.out.println("Total: " + total);
+        System.out.println("iva: " + ivaT);
+        System.out.println("subtotal: " + subtotal);
+    }
+    }
+  
+

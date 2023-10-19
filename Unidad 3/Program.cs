871645ce2a353586206c250eda55859f812352d2
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-18 19:40:43 -0600

    Add files via upload

---
+++ Unidad 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace U3programa1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("Escribe tu edad: ");
+            edad = Convert.ToInt32(Console.ReadLine()); 
+            if (edad >= 18)
+                Console.WriteLine("Eres mayor de edad!!");
+        else
+
+                Console.WriteLine("Eres menor de edad");
+        }
+    }
+}

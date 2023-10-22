de3e9388e3e8ce598835543efe7d4912d1b5b965
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-22 16:10:06 -0600

    Add files via upload

---
+++ Unidad 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace U3programa8 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1, N2;
+            Console.WriteLine("Ingrese el 1er número: ");
+            N1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el 2do número: ");
+            N2 = Convert.ToInt32(Console.ReadLine());
+
+            if (N1 > N2)
+                Console.WriteLine(N1 + " Es mayor que: " + N2);
+            else
+                Console.WriteLine(N1 + " Es menor que: " + N2);
+        }
+    }
+}

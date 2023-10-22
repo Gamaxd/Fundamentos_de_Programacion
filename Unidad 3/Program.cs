506ec8a59ea8a6c650aab062851a99d65647af2d
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-22 16:05:44 -0600

    Add files via upload

---
+++ Unidad 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace U3programa7 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double T;
+            Console.WriteLine("¿Qué temperatura hay hoy?");
+            T = Convert.ToInt32(Console.ReadLine());
+
+            if (T >= 27)
+            {
+                Console.WriteLine("¡Hace calor!!");
+            }
+            else if (T < 27 && T >= 20)
+            {
+                Console.WriteLine("¡Hay clima agradable!");
+            }
+            else
+            {
+                Console.WriteLine("Clima fresco o frio!!!");
+            }
+        }
+    }
+}

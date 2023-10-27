362ad85796ba13558faa5fbc529475c49b491d67
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-27 17:59:35 -0600

    Add files via upload

---
+++ Unidad 3/U3programa17.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u3programa17;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class U3programa17 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

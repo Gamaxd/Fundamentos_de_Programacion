0595903a6177e945bd554b090ed3ccaef428c875
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-30 12:54:00 -0600

    Add files via upload

---
+++ Unidad 3/U3programa14.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u3programa14;
+
+/**
+ *
+ * @author DELL
+ */
+public class U3programa14 {
+
+    public static void main(String[] args) {
+        int c;
+        
+        for (c=1; c<=200; c++)
+        {
+            if (c%2==0)
+            {
+                System.out.println(c);
+            }
+        }
+    }
+}

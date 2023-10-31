0d7e6f9d34b2528179bce0b47f58b3c7311d2d12
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-30 20:06:37 -0600

    Add files via upload

---
+++ Unidad 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace U3programa20 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 1, año = 1, ahorro = 3;
+
+            while (c <= 365)
+            {
+                Console.WriteLine("Su ahorro es de: " + ahorro);
+                ahorro = ahorro * 3;
+                año = ahorro + año;
+                c++;
+            }
+            Console.WriteLine("Su ahorro anual es: " + año);
+        }
+    }
+
+}

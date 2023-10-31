b2d74aa78fbb54c778149c8f30f2fb9f485a8620
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-30 19:22:30 -0600

    Add files via upload

---
+++ Unidad 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace U3programa11 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            for (c = 1; c <= 100; c++)
+            {
+                Console.WriteLine(c);
+            }
+            for (int i = 1; i >= 100; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

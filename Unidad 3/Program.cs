ccd3f2d61cf827fcc28f2a7f77daadfb682fd5a1
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-30 20:02:46 -0600

    Add files via upload

---
+++ Unidad 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace U3programa16 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, h, p, s;
+            while (i <= 20)
+            {
+                Console.WriteLine("Ingrese la cantidad de horas trabajadas: ");
+                h = Convert.ToInt32(Console.ReadLine());    
+                Console.WriteLine("Ingrese el pago de las horas: ");
+                p = Convert.ToInt32(Console.ReadLine());
+                s = h * p;
+                Console.WriteLine("Su sueldo semanal es: " + s);
+                i++;
+            }
+        }
+    }
+}

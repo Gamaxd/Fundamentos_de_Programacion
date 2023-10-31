a1be374971b947216250b0004cf11c3025e7dac4
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-30 19:42:23 -0600

    Update U3programa17.java

--- Unidad 3/U3programa17.java
+++ Unidad 3/U3programa17.java
@@ -11,6 +11,13 @@ package com.mycompany.u3programa17;
 public class U3programa17 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        Scanner sc = new Scanner(System.in);
+        char salir;
+
+        do
+        {
+            System.out.println("Bienvenido a una sala de ejecucion");
+            System.out.println("Escriba n para no condenar a muerte");
+            salir = sc.nextInt().charAt(0);
+    }while(salir !='n');
 }

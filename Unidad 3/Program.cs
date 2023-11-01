a44683412e556e7a21d5ad2a1abe0ae9cd5bfd2d
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-10-31 19:36:38 -0600

    Add files via upload

---
+++ Unidad 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace U3programa22 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0, num;
+            do
+            {
+                Console.WriteLine("Ingrese el valor de num: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                suma = suma + num;
+            } while (num != 0);
+            Console.WriteLine("El resultado es: " + suma);
+        }
+    }
+}

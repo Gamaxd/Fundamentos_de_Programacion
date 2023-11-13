ddaca7b88b980da78e48faa0237a477b9e371d9e
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-11-12 18:44:28 -0600

    Add files via upload

---
+++ Unidad 4/C#/U4programa2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace U4programa2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cal = new int[10 ] {90, 100, 80, 70, 100, 80, 40, 30, 69, 100};
+            double promedio = 0, suma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                suma = suma + cal[i];
+                promedio = suma / 10;
+            }
+            Console.WriteLine("El promedio de tus califiaciones es: " + promedio);
+        }
+    }
+}

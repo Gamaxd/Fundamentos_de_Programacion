d22a680903c8f96af21820748c63d6f3e88be2fe
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-11-12 19:29:47 -0600

    Add files via upload

---
+++ Unidad 4/C#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace U4programa8 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 12, 6, 4, 5, 16, 17, 9, 14, 10 };
+
+            Console.WriteLine("Los numeros pares son: ");
+            foreach (int num in numeros)
+            {
+                if (EsPar(num))
+                {
+                    Console.Write(num + " ");
+                }
+            }
+
+            Console.WriteLine("Los numeros impares son: ");
+            foreach (int num in numeros)
+            {
+                if (!EsPar(num))
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+
+        static bool EsPar(int numero)
+        {
+            return numero % 2 == 0;
+        }
+    }
+
+}

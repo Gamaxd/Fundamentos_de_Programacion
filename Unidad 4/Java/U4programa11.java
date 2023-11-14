2d5822c57000560030e7406f547dc3f49493cf5d
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-11-13 19:54:42 -0600

    Add files via upload

---
+++ Unidad 4/Java/U4programa11.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u4programa11;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class U4programa11 {
+
+    public static void main(String[] args) {
+        /*int[][] cal = new int [3] [3];
+        
+        cal[0][0] = 5;
+        cal[0][1] = 7;
+        cal[0][2] = 9;
+        cal[1][0] = 3;
+        cal[1][1] = 6;
+        cal[1][2] = 8;
+        cal[2][0] = 2;
+        cal[2][1] = 3;
+        cal[2][2] = 6;*/
+        int[][] cal = { {5, 7, 9}, {3, 6, 8}, {2, 3, 6} }; 
+        
+        System.out.println(cal[0][0]);
+        System.out.println(cal[0][1]);
+        System.out.println(cal[0][2]);
+        System.out.println(cal[1][0]);
+        System.out.println(cal[1][1]);
+        System.out.println(cal[1][2]);
+        System.out.println(cal[2][0]);
+        System.out.println(cal[2][1]);
+        System.out.println(cal[2][2]);
+    }
+}

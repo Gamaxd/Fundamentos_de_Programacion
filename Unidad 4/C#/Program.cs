1cfee22ac31444ec3c24591b9968d0fc5e0c5df0
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-11-22 11:05:20 -0600

    Add files via upload

---
+++ Unidad 4/C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace U4programa15 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matriz = new int[3];
+            matriz[0] = 3;
+            matriz[1] = 5;
+            matriz[2] = 2;
+
+            Console.WriteLine("Contenido del arreglo");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(matriz[i]);
+
+            }
+            double media;
+            media = (matriz[0] + matriz[1] + matriz[2]) / 3;
+            Console.WriteLine("La media de los arreglos en la matriz es: " + media);
+        }
+    }
+}

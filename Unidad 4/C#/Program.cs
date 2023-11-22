93f7f1113f910430a5d4e0d2dc2cf7c07e5e5d70
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-11-22 11:04:39 -0600

    Add files via upload

---
+++ Unidad 4/C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace U4prorama14 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matriz = new int[3];
+            matriz[0] = 3;
+            matriz[1] = 5;
+            matriz[2] = 2;
+
+            Console.WriteLine("Contenido del arreglo");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(matriz[i]);
+
+            }
+            int suma;
+            suma = matriz[0] + matriz[1] + matriz[2];
+            Console.WriteLine("La suma de los arreglos en la matriz es: " + suma);
+        }
+    }
+}

8315a3f1207c78739b80676d8607a4d3b8382aee
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-12-09 19:39:39 -0600

    Add files via upload

---
+++ Unidad 5/C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace U5programa10 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int longitud = 8;
+            String contrasena = aleatorio(longitud);
+            Console.WriteLine("password: " + contrasena);
+
+
+        }
+        public static String aleatorio(int lon)
+        {
+            String caracteres = "ABCDEFGHIJK123456789*-_.";
+            Random random = new Random();
+            StringBuilder cadena = new StringBuilder(lon);
+            for (int i = 0; i < lon; i++)
+            {
+                int indiceCaracter = random.Next(caracteres.LengthL());
+                cadena.Append(caracteres.CharAt(indiceCaracter));
+            }
+            return cadena.ToString();
+        }
+    }
+}

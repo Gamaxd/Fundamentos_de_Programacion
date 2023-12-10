7155bf145f2af07a57e3a9949faeca45b4adb58e
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-12-09 19:26:17 -0600

    Add files via upload

---
+++ Unidad 5/C#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace U5programa6 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+            for (int i = 0; i <= 15; i++)
+            {
+                Console.WriteLine(a + " ");
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}

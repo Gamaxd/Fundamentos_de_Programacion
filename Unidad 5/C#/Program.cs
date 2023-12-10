e930948c9124c9115f82790cee34b1364e14ba1c
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-12-09 19:34:30 -0600

    Add files via upload

---
+++ Unidad 5/C#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace U5programa8 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dado();
+        }
+        public static void dado()
+        {
+            Random aleatorio = new Random();
+            int G = aleatorio.Next(6);
+            Console.WriteLine("Su numero es: " + G);
+        }
+    }
+}

34f97fee6dcb51cae2f082d441cd584ad7c7e36a
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2023-12-09 19:23:54 -0600

    Add files via upload

---
+++ Unidad 5/C#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace U5programa5 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una palabra completa: ");
+            String palabra = Convert.ToString(Console.ReadLine);
+            Console.WriteLine(palabra + " tiene un total de: " + palabra(palabra) + " letras");
+        }
+        public static int palabra(String x)
+        {
+            int palabra = x.Length();
+            return palabra;
+        }
+    }
+}

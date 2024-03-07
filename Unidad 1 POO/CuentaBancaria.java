4781d7df9dd274a7d4e93787a0574fe28fe9a4cd
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2024-03-07 09:47:50 -0600

    Update CuentaBancaria.java

--- Unidad 1 POO/CuentaBancaria.java
+++ Unidad 1 POO/CuentaBancaria.java
@@ -1,83 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package banco_1;
+import java.util.Scanner;
 
-/**
- *
- * @author SALA PAFP 27
- */
-public class CuentaBancaria {
-    // Crear clase
-    // Definir atributos
-    int numCuenta;
-    String nomCliente;
-    float saldo;
-    int cvv;
-    // Definir el constructor.
+public class Banco_1 {
 
-    public CuentaBancaria(int numCuenta, String nomCliente, float saldo, int cvv) {
-        this.numCuenta = numCuenta;
-        this.nomCliente = nomCliente;
-        this.saldo = saldo;
-        this.cvv = cvv;
-    }
+public static void main(String[] args) {
+//crear un objeto de la clase CuentaBancaria
+//ejecutar todos los métodos desde el objeto
+int numCuenta;
+String nombCliente;
+float saldo;
+int cvv;
+double cantidad;
+CuentaBancaria cuentaB = null;
+CuentaBancaria cuentaB2 = null;
+Scanner entrada = new Scanner(System.in);
+int opc;
+do {
+System.out.println("Seleccione una opcion");
+System.out.println("1. Crear 2 cuentas");
+System.out.println("2. Depositar");
+System.out.println("3. Retirar");
+System.out.println("4. Consultar saldo");
+System.out.println("5. Mostrar datos ");
+System.out.println("6. Salir");
+opc=entrada.nextInt();entrada.nextLine();
+switch(opc){
+case 1: // crear la cuenta
+System.out.println("¿Número de cuenta? ");
+numCuenta = entrada.nextInt(); //lee un entero
+entrada.nextLine(); //limpia el buffer
+System.out.println("¿Nombre del cliente? ");
+nombCliente = entrada.nextLine();//lee un String
+System.out.println("¿Saldo inicial? ");
+saldo = entrada.nextFloat();//lee un float
+System.out.println("¿CVV? ");
+cvv = entrada.nextInt();
+cuentaB = new CuentaBancaria(numCuenta,nombCliente,saldo,cvv);
+System.out.println("¿Número de cuenta? ");
+numCuenta = entrada.nextInt(); //lee un entero
+entrada.nextLine(); //limpia el buffer
+System.out.println("¿Nombre del cliente? ");
+nombCliente = entrada.nextLine();//lee un String
+System.out.println("¿Saldo inicial? ");
+saldo = entrada.nextFloat();//lee un float
+System.out.println("¿CVV? ");
+cvv = entrada.nextInt();
+cuentaB2 = new CuentaBancaria(numCuenta,nombCliente,saldo,cvv);
+break;
+case 2: // depositar en la cuenta
+System.out.println("¿Qué cantidad desea depositar? ");
+cantidad = entrada.nextDouble();
+cuentaB.depositar(cantidad);
+cuentaB.calcularComision();
+cuentaB.calcularComision(cantidad);
+break;
+case 3: // retirar de la cuenta
+System.out.println("¿Qué cantidad desea retirar? ");
+cantidad = entrada.nextDouble();
+cuentaB.retirar(cantidad);
+break;
+case 4: // consultar saldo
+System.out.println("El saldo actual es "+cuentaB.consultarSaldo());
+break;
+case 5: //Mostrar los datos de la cuenta
+System.out.println(cuentaB.toString());
+System.out.println("Número de cuenta "+
+cuentaB.getNumCuenta());
+case 6:
+System.out.println("Adios");
+break;
+default: System.out.println("Opción no válida");
+}
+}while(opc!=6);
 
-    public int getNumCuenta() {
-        return numCuenta;
-    }
-
-    public String getNomCliente() {
-        return nomCliente;
-    }
-
-    public float consultarSaldo() {
-        return saldo;
-    }
-
-    public int getCvv() {
-        return cvv;
-    }
-
-    public void setNumCuenta(int numCuenta) {
-        this.numCuenta = numCuenta;
-    }
-
-    public void setNomCliente(String nomCliente) {
-        this.nomCliente = nomCliente;
-    }
-
-    public void setSaldo(float saldo) {
-        this.saldo = saldo;
-    }
-
-    public void setCvv(int cvv) {
-        this.cvv = cvv;
-    }
-
-    @Override
-    public String toString() {
-        return "CuentaBancaria{" + "numCuenta=" + numCuenta + ", nomCliente=" + nomCliente + ", saldo=" + saldo + ", cvv=" + cvv + '}';
-    }
-    
-    // metodo depositar recibe una cantidad que se sumara al saldo
-    void depositar(double cantidad){
-        saldo += cantidad;
-        // la instruccion equivale a saldo = saldo + cantidad
-    }
-    //El metodo retirar recibe una cantidad que se va a restar al saldo
-    void retirar(double cantidad){
-        if (haySaldo(cantidad))
-            saldo -= cantidad;
-        else
-            System.out.println("Fondos insuficientes");
-       
-    }
-    // El metodo haySaldo retorna verdadero si el saldo es mayor o igual a una cantidad o retorna a falso en caso contrario.
-    boolean haySaldo(double cantidad){
-        if (saldo >= cantidad)
-            return true;
-        else
-            return false;
-    }
-    }
+System.out.println(cuentaB.toString());
+}
+}

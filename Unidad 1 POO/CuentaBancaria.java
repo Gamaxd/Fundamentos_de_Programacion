7a5b7c04ca5af7321d2ca0ed9ad109d8b122fc07
Gamaxd <146400833+Gamaxd@users.noreply.github.com>
2024-03-07 09:49:16 -0600

    Update CuentaBancaria.java

--- Unidad 1 POO/CuentaBancaria.java
+++ Unidad 1 POO/CuentaBancaria.java
@@ -1,78 +1,60 @@
 package banco_1;
-import java.util.Scanner;
 
-public class Banco_1 {
+public class CuentaBancaria {
+//1. Crear la clase
+//2. Definir los atributos
+   private int numCuenta;
+   String nombCliente;
+   float saldo;
+   int cvv;
+//3. Definir el constructor
 
-public static void main(String[] args) {
-//crear un objeto de la clase CuentaBancaria
-//ejecutar todos los métodos desde el objeto
-int numCuenta;
-String nombCliente;
-float saldo;
-int cvv;
-double cantidad;
-CuentaBancaria cuentaB = null;
-CuentaBancaria cuentaB2 = null;
-Scanner entrada = new Scanner(System.in);
-int opc;
-do {
-System.out.println("Seleccione una opcion");
-System.out.println("1. Crear 2 cuentas");
-System.out.println("2. Depositar");
-System.out.println("3. Retirar");
-System.out.println("4. Consultar saldo");
-System.out.println("5. Mostrar datos ");
-System.out.println("6. Salir");
-opc=entrada.nextInt();entrada.nextLine();
-switch(opc){
-case 1: // crear la cuenta
-System.out.println("¿Número de cuenta? ");
-numCuenta = entrada.nextInt(); //lee un entero
-entrada.nextLine(); //limpia el buffer
-System.out.println("¿Nombre del cliente? ");
-nombCliente = entrada.nextLine();//lee un String
-System.out.println("¿Saldo inicial? ");
-saldo = entrada.nextFloat();//lee un float
-System.out.println("¿CVV? ");
-cvv = entrada.nextInt();
-cuentaB = new CuentaBancaria(numCuenta,nombCliente,saldo,cvv);
-System.out.println("¿Número de cuenta? ");
-numCuenta = entrada.nextInt(); //lee un entero
-entrada.nextLine(); //limpia el buffer
-System.out.println("¿Nombre del cliente? ");
-nombCliente = entrada.nextLine();//lee un String
-System.out.println("¿Saldo inicial? ");
-saldo = entrada.nextFloat();//lee un float
-System.out.println("¿CVV? ");
-cvv = entrada.nextInt();
-cuentaB2 = new CuentaBancaria(numCuenta,nombCliente,saldo,cvv);
-break;
-case 2: // depositar en la cuenta
-System.out.println("¿Qué cantidad desea depositar? ");
-cantidad = entrada.nextDouble();
-cuentaB.depositar(cantidad);
-cuentaB.calcularComision();
-cuentaB.calcularComision(cantidad);
-break;
-case 3: // retirar de la cuenta
-System.out.println("¿Qué cantidad desea retirar? ");
-cantidad = entrada.nextDouble();
-cuentaB.retirar(cantidad);
-break;
-case 4: // consultar saldo
-System.out.println("El saldo actual es "+cuentaB.consultarSaldo());
-break;
-case 5: //Mostrar los datos de la cuenta
-System.out.println(cuentaB.toString());
-System.out.println("Número de cuenta "+
-cuentaB.getNumCuenta());
-case 6:
-System.out.println("Adios");
-break;
-default: System.out.println("Opción no válida");
+   public CuentaBancaria(int numCuenta, String nombCliente, float saldo,int cvv) {
+     this.numCuenta = numCuenta;
+     this.nombCliente = nombCliente;
+     this.saldo = saldo;
+     this.cvv = cvv;
+   }
+
+public int getNumCuenta() {
+return numCuenta;
 }
-}while(opc!=6);
 
-System.out.println(cuentaB.toString());
+public void setNumCuenta(int numCuenta) {
+this.numCuenta = numCuenta;
 }
+
+public String getNombCliente() {
+return nombCliente;
+}
+
+public void setNombCliente(String nombCliente) {
+this.nombCliente = nombCliente;
+}
+
+public float consultarSaldo() {
+return saldo;
+}
+
+public void setSaldo(float saldo) {
+this.saldo = saldo;
+}
+
+public int getCvv() {
+return cvv;
+}
+
+public void setCvv(int cvv) {
+this.cvv = cvv;
+}
+// método sobre-escrito
+@Override
+public String toString() {
+return "CuentaBancaria{" + "numCuenta="
++ numCuenta + ", nombCliente="
++ nombCliente + ", saldo="
++ saldo + ", cvv=" + cvv + '}';
+}
+//método sobrecargado
+
 }
